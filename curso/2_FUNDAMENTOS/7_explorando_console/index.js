c2592b1a8c3303d1655e428d75674d304cbfed7b
isaac-ReDev <isacbs2017ce@gmail.com>
2022-09-25 16:18:24 -0300

    explorando o console.

---
+++ curso/2_FUNDAMENTOS/7_explorando_console/index.js
@@ -0,0 +1,19 @@
+const a = 10;
+const b = "be";
+const  c = false;
+
+console.log(a,b,c,)
+console.log("CONSOLE.COUNT('***')")
+
+console.count("o valor de a e ", a + " couter")
+console.count("o valor de a e ", a + " couter")
+console.count("o valor de a e ", a + " couter")
+console.count("o valor de a e ", a + " couter")
+console.count("o valor de a e ", a + " couter")
+
+//& variavel entre strings
+console.log("CONSOLE.COUNT('***')")
+
+setTimeout(() => {
+    console.clear();
+},2000)
