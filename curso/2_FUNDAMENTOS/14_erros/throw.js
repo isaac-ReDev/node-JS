d67e8460306e54f82305539a6e53ead67f391ef9
isaac-ReDev <isacbs2017ce@gmail.com>
2022-09-28 11:17:55 -0300

    Node.JS erros.

---
+++ curso/2_FUNDAMENTOS/14_erros/throw.js
@@ -0,0 +1,8 @@
+const x = "10"
+
+
+if(!Number.isInteger(x)){
+    throw new Error("Ovalor de x naso e um numero inteiro!");
+};
+
+console.log("Continuando o codigo. . .");
---
+++ curso/2_FUNDAMENTOS/14_erros/try_catch.js
@@ -0,0 +1,8 @@
+const x = 5;
+
+try{
+    x + 2;
+    console.log("Otrye deu certo!")
+} catch(error){
+    console.log(error)
+}
