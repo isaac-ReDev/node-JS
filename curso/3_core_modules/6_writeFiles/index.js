a9e02cdd03405ba9be693030771ac44c1f0df4d2
isaac-ReDev <isacbs2017ce@gmail.com>
2022-10-01 10:15:16 -0300

    Atualizando arquivo com FS appendFile.

---
+++ curso/3_core_modules/6_writeFiles/index.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs   = require("fs");
+
+const port = 3000;
+
+const server = http.createServer((req, resp) => {
+    const urlInfo = require("url").parse(req.url, true);
+    const name    = urlInfo.query.name
+
+    if(!name){
+        fs.readFile("./msg.html", function(error, data){
+            try{
+                resp.writeHead(200, {"Content-Type": "text/html"})
+                resp.write(data)
+                return resp.end()
+            }
+            catch{
+                console.log(error);
+            }
+        })
+    }
+    else{
+        fs.writeFile("arq.txt", name ,function(error,data){
+            resp.writeHead(302, {
+                Location: '/',
+            });
+            return resp.end()
+        })
+    }
+}  )
+
+server.listen(port, () => {
+    console.log("servidor rodadando na porta ", port)
+});
